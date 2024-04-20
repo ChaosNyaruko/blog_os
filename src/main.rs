16ce3cbe605516b571d4731891344e790a535cc7
ChaosNyaruko <cabbageparadise@gmail.com>
2024-04-20 10:35:28 +0800

    construct a vga module

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 // TODO:https://github.com/phil-opp/blog_os/issues/1249#issuecomment-2005158679
 // Now, if you are on MacOS newer than than version 14, and you are using Qemu, listen up.
 // There is a window-sizing bug in Qemu that will shift text up, preventing you from being able to see some of it.
@@ -12,14 +14,7 @@ static HELLO: &[u8] =
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // VGA text buffer format: 2bytes=ASCII+colour
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
     loop {}
 }
 
