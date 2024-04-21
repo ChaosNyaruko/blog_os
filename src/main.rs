2126148293cd5997a3cbb662e2e7b5af71f9f25c
ChaosNyaruko <cabbageparadise@gmail.com>
2024-04-21 17:49:27 +0800

    implement global interface

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,16 @@ use core::panic::PanicInfo;
 
 mod vga_buffer;
 
-// TODO:https://github.com/phil-opp/blog_os/issues/1249#issuecomment-2005158679
-// Now, if you are on MacOS newer than than version 14, and you are using Qemu, listen up.
-// There is a window-sizing bug in Qemu that will shift text up, preventing you from being able to see some of it.
-// So I prefix it with a LONGLONGLONGLONG string to let the "Hello World" be seen!
-static HELLO: &[u8] =
-    b"oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooopppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp\nHello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
-    loop {}
+    println!("Hello world{}", "!");
+    loop {
+        panic!("Whoops!");
+    }
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -115,18 +115,31 @@ impl fmt::Write for Writer {
     }
 }
 
-pub fn print_something() {
-    use core::fmt::Write;
-    let mut writer = Writer {
+use lazy_static::lazy_static;
+use spin::Mutex;
+
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
+    });
+}
 
-    writer.write_byte(b'H');
-    writer.write_string("ello ");
-    write!(writer, "The numbers are {} and {}", 42, 1.0 / 3.0).unwrap();
-    write!(writer, "veryveryverylonglonglonglonglonglonglonglonglong").unwrap();
-    write!(writer, "\n").unwrap();
-    write!(writer, "blala").unwrap();
+#[macro_export]
+macro_rules! print {
+    // print("", "x", a, b)
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
 }
