019de6a7bebfe32fbe6200f51f55a7a4b73e23fd
ChaosNyaruko <cabbageparadise@gmail.com>
2024-05-11 09:51:50 +0800

    integration tests for out kernel

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,34 +5,24 @@
 #![reexport_test_harness_main = "test_main"]
 
 use blog_os::println;
+use blog_os::serial_println;
 use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello world{}", "!");
-
-    #[cfg(test)]
     test_main();
 
-    loop {
-        // panic!("Whoops!");
-    }
-}
-
-#[test_case]
-fn xx() {
-    assert_eq!(1, 1);
-}
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", _info);
     loop {}
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info)
+    // serial_println!("it panics");
+    // loop {}
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
