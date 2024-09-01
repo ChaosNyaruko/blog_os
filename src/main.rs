fcd5446a3f384c2135bb517952de91dc7abcfae7
ChaosNyaruko <cabbageparadise@gmail.com>
2024-09-01 20:10:13 +0800

    create new mappings and bootinfo frame allocator

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@
 use blog_os::{memory::active_level_4_table, println};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use x86_64::{structures::paging::PageTable, VirtAddr};
+use x86_64::{
+    structures::paging::{Page, PageTable},
+    VirtAddr,
+};
 
 entry_point!(kernel_main);
 
@@ -17,35 +20,39 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     blog_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    use blog_os::memory::BootInfoFrameAllocator;
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
     let addresses = [
         // vga buffer identity-mapped -> io
         0xb8000,
         // some code page
-        0x201008,
+        // 0x201008,
         // some stack page
-        0x0100_0020_1a10,
+        // 0x0100_0020_1a10,
         // real physical 0 start
-        boot_info.physical_memory_offset, //-> 0
-        // our 0x1000
-        0x1000,
-        // not mapped, e.g. 0 -> null pointer ---> huge page panic
-        0xdeadbeaf, // -> None
-        0,          // -> None
+        // boot_info.physical_memory_offset, //-> 0
+        0x0,
+        0xdeadbeaf000,
     ];
 
-    let mapper = unsafe { memory::init(phys_mem_offset) };
     use blog_os::memory;
     use x86_64::{structures::paging::Translate, VirtAddr};
     for &address in &addresses {
         let virt = VirtAddr::new(address);
         let phys = mapper.translate_addr(virt);
         println!("{:?} -> {:?}", virt, phys);
-        unsafe {
-            let x = *(address as *mut u8);
-            println!("{:?} contains {:}", addresses, x);
-        };
     }
+
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+
+    // literal "New!"
+    // x86 is little-endian
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     #[cfg(test)]
     test_main();
--- src/memory.rs
+++ src/memory.rs
@@ -1,5 +1,10 @@
-use x86_64::structures::paging::OffsetPageTable;
+use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
+use x86_64::structures::paging::{
+    FrameAllocator, Mapper, OffsetPageTable, Page, PhysFrame, Size4KiB,
+};
 use x86_64::{structures::paging::PageTable, PhysAddr, VirtAddr};
+
+use crate::println;
 
 // 给到一个能从虚拟空间访问的4级页表地址
 // CR3->Physical->Virtual->通过虚拟地址的访问，找到对应的4级页表
@@ -20,37 +25,75 @@ pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static>
     OffsetPageTable::new(level_4_table, physical_memory_offset)
 }
 
-// pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
-//     translate_addr_inner(addr, physical_memory_offset)
-// }
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    // VGA: memory-mapped IO 0xb8000
+    // Virtual 0xb8000 -> Phy:0xb8000
+    // Virtual 0x00000 -> Phy:0xb8000
+    // 需要Level 1 table 新添一项
+    // 0x0所在的页，由于bootloader的存在，其一级页表已经出现在物理内存里，而且被映射过
+    // 0->4KiB本身没有被映射，但是其所在一级页表的其他页被映射过，那么这里就不需要一个额外的物理内存分配，来容纳新的一张页表
 
-// fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
-//     use x86_64::registers::control::Cr3;
-//     use x86_64::structures::paging::page_table::FrameError;
+    use x86_64::structures::paging::PageTableFlags as Flags;
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000)); //  184 * 4KiB
+    let flags = Flags::PRESENT | Flags::WRITABLE;
 
-//     let (level_4_table_frame, _) = Cr3::read();
+    let map_to_result = unsafe {
+        // FIXME: just for testing
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    // TLB cache
+    // write `page` -> `frame` to TLB
+    map_to_result.expect("map_to failed").flush();
+}
 
-//     let table_indexes = [
-//         addr.p4_index(),
-//         addr.p3_index(),
-//         addr.p2_index(),
-//         addr.p1_index(),
-//     ];
+pub struct EmptyFrameAllocator;
 
-//     let mut frame = level_4_table_frame;
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<x86_64::structures::paging::PhysFrame<Size4KiB>> {
+        None
+    }
+}
 
-//     for &index in &table_indexes {
-//         let virt = physical_memory_offset + frame.start_address().as_u64();
-//         let table_ptr: *const PageTable = virt.as_ptr();
-//         let table = unsafe { &*table_ptr };
+pub struct BootInfoFrameAllocator {
+    memory_map: &'static MemoryMap,
+    next: usize,
+}
 
-//         let entry = &table[index];
-//         frame = match entry.frame() {
-//             Ok(frame) => frame,
-//             Err(FrameError::FrameNotPresent) => return None,
-//             Err(FrameError::HugeFrame) => panic!("huge pages not supported"),
-//         };
-//     }
+impl BootInfoFrameAllocator {
+    pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
+        BootInfoFrameAllocator {
+            memory_map,
+            next: 0,
+        }
+    }
 
-//     Some(frame.start_address() + u64::from(addr.page_offset()))
-// }
+    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
+        // a region may consist of several frames.
+        let regions = self.memory_map.iter();
+        let usable_regions = regions.filter(|r| r.region_type == MemoryRegionType::Usable);
+
+        let addr_ranges = usable_regions.map(|r| r.range.start_addr()..r.range.end_addr());
+
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+        frame_addresses.map(|addr| {
+            println!("addr:{:?}", addr);
+            panic!("stops here");
+            // [0x1 0x2 0x3 ......4KiB + 1 )
+            // 4KiB+1 +2 xxxxxxxxx8KiB + 1)
+            // ...
+            PhysFrame::containing_address(PhysAddr::new(addr))
+        })
+    }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+        let frame = self.usable_frames().nth(self.next);
+        self.next += 1;
+        frame
+    }
+}
